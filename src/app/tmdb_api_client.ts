d07163f51ac113db422f0621a2aecf3f2585c925
s_ms_on_github <90606181+WhileWorkingdoLearn@users.noreply.github.com>
2024-08-17 22:51:11 +0200

    v 0.1.2
    Server

--- src/app/tmdb_api_client.ts
+++ src/app/tmdb_api_client.ts
@@ -18,13 +18,70 @@ export interface IMovieItem {
   title: string;
 }
 
-export default function getMoviesFromAPI<R extends {items:P},P>(url:string,key:string): Promise<P |never[]> {
+
+
+export interface IMovieTrailerRespone{
+  id:number,
+  results:IMovieTrailer[]
+}
+
+export interface IMovieTrailer {
+  iso_639_1: string,
+  iso_3166_1: string,
+  name: string,
+  key: string,
+  site: string,
+  size: number,
+  type: string,
+  official: boolean,
+  published_at: string,
+  id: string
+}
+
+
+
+export function getMoviesFromAPI<R extends {items:P},P>(url:string,key:string): Promise<P> {
   const promise = axios.get<R>(url,{
     timeout: 3000,
     headers: {
       Authorization: `Bearer ${key}`,
     },
   });
-  const dataPromise : Promise<P |never[]> = promise.then((response:AxiosResponse<R>) => response.data.items).catch(()=> []);
+  const dataPromise : Promise<P> = promise.then((response:AxiosResponse<R>) => response.data.items);
   return dataPromise;
+}
+
+export function getTrailersFromAPI<R extends {results:P},P>(url:string,key:string): Promise<P> {
+  const promise = axios.get<R>(url,{
+    timeout: 3000,
+    headers: {
+      Authorization: `Bearer ${key}`,
+    },
+  });
+  const dataPromise : Promise<P> = promise.then((response:AxiosResponse<R>) => response.data.results);
+  return dataPromise;
+}
+
+const youtubePath = process.env.YOUTUBE_VIDEO || '';
+const youtubeImage = process.env.YOUTUBE_IMAGE || '';
+
+export function getTrailers(url:string,apiReaderKey:string,filterFor:string):{provider:string,trailerPaths:string[],imagePaths:string[]}{
+
+  const trailers = getTrailersFromAPI<IMovieTrailerRespone,IMovieTrailer[]>(url,apiReaderKey);
+  let trailerList:string[] = [];
+  let imageList:string[] = [];
+
+   const data = trailers.then((data)=>{ 
+       const filteredData = data.filter((value:IMovieTrailer) => {
+        return value.type === 'Trailer' && value.site === filterFor;
+      });
+      trailerList = filteredData.map((trailer)=> {
+        return youtubePath + trailer.key;
+      });
+      imageList = filteredData.map((trailer)=> {
+        return youtubeImage + trailer.key + '/default.jpg';
+      });
+  });
+
+  return {provider:filterFor,trailerPaths:trailerList,imagePaths:imageList}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from "cors";
-import getMoviesFromAPI, { IMovieItem, IMovieListResponse } from './app/tmdb_api_client';
+import {getMoviesFromAPI, getTrailers, IMovieItem, IMovieListResponse, } from './app/tmdb_api_client';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,23 +8,37 @@ const app = express();
 const port = process.env.PORT || 1000;
 const cachedMovies :Map<string,IMovieItem[]> = new Map();
 
-const path = process.env.TMDB_MOVIE_LIST || '';
-const key = process.env.TMDB_READERKEY || '';
-
+const movieListPath = process.env.TMDB_MOVIE_LIST || '';
+const apiReaderKey = process.env.TMDB_READERKEY || '';
+const apiKey = process.env.TMDB_APIKEY || '';
 
 app.use(cors());
+
 app.get('/movies/:id',async (req, res) => {
     if(cachedMovies.has(req.params.id)){
       console.log("in Cache");
       res.json(cachedMovies.get(req.params.id));
     } else {
-      const movies = getMoviesFromAPI(path,key);
+      const movies = getMoviesFromAPI<IMovieListResponse,IMovieItem[]>(movieListPath,apiReaderKey);
       movies.then((data)=> res.json(data));
       movies.catch((err) => {
-        res.send('Hello, TypeScript with Express!');
+        res.send('Internal Server Error');
       })
     }
 
+});
+
+
+const trailerListPath = process.env.TMDB_TRAILER_LIST || '';
+
+app.get('/movies/trailers/:id', async(req, res) => {
+    const url = trailerListPath + req.params.id + "/videos";
+
+    console.log(url);
+    
+    const trailers =  getTrailers(url,apiReaderKey,'YouTube');
+
+    res.json(trailers);
 });
 
 app.get('/movies/pictures', (req, res) => {
@@ -34,12 +48,13 @@ app.get('/movies/pictures', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-  
+
   //First Parameter Interface needs to have 'items' property of type P
-  const movies = getMoviesFromAPI<IMovieListResponse,IMovieItem[]>(path,key);
+  const movies = getMoviesFromAPI<IMovieListResponse,IMovieItem[]>(movieListPath,apiReaderKey);
   movies.then((data)=>{ cachedMovies.set("123456789",data);
     console.log("Movies loaded");
   });
 });
 
 
+export default app;
